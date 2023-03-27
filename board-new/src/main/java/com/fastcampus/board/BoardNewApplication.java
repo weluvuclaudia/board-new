afac8afc4a9064f37ac756815ac720a246181d9f
fastcamedu <claudiatheprofessional@gmail.com>
2023-03-28 00:32:43 +0900

    #6-스트링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술은
    *스프링웹
    *액츄에이터
    *devtools
    *롬복

---
+++ board-new/src/main/java/com/fastcampus/board/BoardNewApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardNewApplication.class, args);
+    }
+
+}
